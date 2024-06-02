3f1d707e7ba3d1942a017748a8cfa6cbf6040c8f
b1gmcdaddy <bobtangpuz@gmail.com>
2024-06-02 14:01:50 +0800

    added: ParishionerNavBar & folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/parishioner/Home'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-     
-    </>
+    <main>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+  </main>
   )
 }
 
---
+++ client/src/pages/parishioner/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavParishioner from '../../components/NavParishioner'
+
+
+const Home = () => {
+  return (
+    <>
+    <NavParishioner />
+    </>
+  )
+}
+
+export default Home
