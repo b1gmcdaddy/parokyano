f7c2f6280386f68ed537e8ce708293a2892ec217
b1gmcdaddy <bobtangpuz@gmail.com>
2024-06-06 22:33:58 +0800

    request notice path url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/parishioner/Home'
 import Certificates from './pages/parishioner/Certificate'
 import About from './pages/parishioner/About'
 import MassSelection from './pages/parishioner/MassSelection'
+import Baptism from './pages/parishioner/Baptism'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Route path='/certificates' element={<Certificates />} />
       <Route path='/about' element={<About />} />
       <Route path='/mass-selection' element={<MassSelection/>}/>
+      <Route path='/baptism' element={<Baptism />}/>
     </Routes>
   </main>
   )
---
+++ client/src/pages/parishioner/Baptism.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Baptism = () => {
+  return (
+    <div>Baptism</div>
+  )
+}
+
+export default Baptism
