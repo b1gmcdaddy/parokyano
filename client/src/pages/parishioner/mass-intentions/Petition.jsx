1e9dbdb97c0ad96993f6ed30bc6c6f9360fa0fda
CJ <noobclyde21@gmail.com>
2024-06-24 14:18:03 +0800

    initial backend setup for petition

--- client/src/pages/parishioner/mass-intentions/Petition.jsx
+++ client/src/pages/parishioner/mass-intentions/Petition.jsx
@@ -26,6 +26,27 @@ const Petition = () => {
 
   const [captchaValue, setCaptchaValue] = useState(null);
 
+    const [data, setData] = useState({
+        intention_details: '',
+        offered_by: '',
+        mass_date: '',
+        mass_time: '',
+        payment_method: '',
+        donation_amount: ''
+    })
+
+    //show change in data
+    console.log(data)
+
+    //change value of data with whatever user is typing
+    const handleChange = (e) => {
+        setData({...data, [e.target.name]: e.target.value})
+    }
+ 
+    const handleSubmit = () => {
+        alert("submit!")
+    }
+
     const handleCaptchaChange = (value) => {
         setCaptchaValue(value)
     }
@@ -47,24 +68,51 @@ const Petition = () => {
             <h1 align='center' className="font-bold text-md font-[Arial] mb-8">Please input the following</h1>
 
             <Container maxWidth="md" sx={{ marginBottom: '50px' }}>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <Grid container spacing={4}>
+
                         <Grid item xs={12} sm={12}>
                             <label>Write Petition here:</label>
-                            <TextField fullWidth variant="outlined" size="medium" sx={inputstlying} multiline maxRows={Infinity} required />
+                            <TextField name="intention_details" 
+                                onChange={handleChange} 
+                                fullWidth 
+                                variant="outlined" 
+                                size="medium" 
+                                sx={inputstlying} 
+                                multiline maxRows={Infinity} />
                         </Grid>
                         
                         <Grid item xs={12} sm={4}>
                             <label>Offered by:</label>
-                            <TextField fullWidth variant="outlined" size="small" sx={inputstlying} required />
+                            <TextField name="offered_by"
+                                onChange={handleChange}
+                                fullWidth 
+                                variant="outlined" 
+                                size="small" 
+                                sx={inputstlying} />
                         </Grid>
+
                         <Grid item xs={12} sm={4}>
                             <label>Mass Date:</label>
-                            <TextField fullWidth variant="outlined" type="date" size="small" sx={inputstlying} required />
+                            <TextField name="mass_date"
+                                onChange={handleChange} 
+                                fullWidth 
+                                variant="outlined" 
+                                type="date" 
+                                size="small" 
+                                sx={inputstlying} />
                         </Grid>
+
                         <Grid item xs={12} sm={4}>
                             <label>Time Slot:</label>
-                            <TextField fullWidth select variant="outlined" size="small" sx={inputstlying} required >
+                            <TextField name="mass_time"
+                                onChange={handleChange} 
+                                value={data.mass_time}
+                                fullWidth 
+                                select 
+                                variant="outlined" 
+                                size="small" 
+                                sx={inputstlying} >
                             <MenuItem value="time slot 1">time slot 1</MenuItem>
                             <MenuItem value="sdf">time slot 2</MenuItem>
                             </TextField>
@@ -73,14 +121,27 @@ const Petition = () => {
                         
                         <Grid item xs={12} sm={4}>
                             <label>Payment Method:</label>
-                            <TextField fullWidth select variant="outlined" size="small" sx={inputstlying} required>
+                            <TextField name="payment_method" 
+                                value={data.payment_method}
+                                onChange={handleChange} 
+                                fullWidth 
+                                select 
+                                variant="outlined" 
+                                size="small" 
+                                sx={inputstlying} >
                             <MenuItem value="cash">Cash</MenuItem>
                             <MenuItem value="gcash">GCash</MenuItem>
                             </TextField>
                         </Grid>
+
                         <Grid item xs={12} sm={4}>
                             <label>Donation Amount:</label>
-                            <TextField fullWidth variant="outlined" size="small" sx={inputstlying} required />
+                            <TextField name="donation_amount"
+                                onChange={handleChange} 
+                                fullWidth 
+                                variant="outlined" 
+                                size="small" 
+                                sx={inputstlying} />
                         </Grid>
 
 
