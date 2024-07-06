44c0926f4054aea5155f1119a667160490cecc6c
b1gmcdaddy <bobtangpuz@gmail.com>
2024-07-07 02:43:49 +0800

    finished: staff dashboard & staff nav

--- client/src/pages/staff/StaffDashboard.jsx
+++ client/src/pages/staff/StaffDashboard.jsx
@@ -3,9 +3,9 @@ import NavStaff from '../../components/NavStaff';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Paper from '@mui/material/Paper';
-import { Button, Typography } from '@mui/material';
+import { Button, Typography, Container } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowDown, faChurch, faStamp, faHandsPraying } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight, faChurch, faStamp, faHandsPraying } from '@fortawesome/free-solid-svg-icons';
 
 const sideBarWidth = 240;
 
@@ -18,6 +18,8 @@ const sampleStatistics = {
 const sampleRecentServices = [
   {type: "Funeral Mass", requestor: "John Doe", dateRequested: "April 11, 2024"},
   {type: "Baptism", requestor: "John Doe", dateRequested: "April 08, 2024"},
+  {type: "Wake Mass", requestor: "John Doe", dateRequested: "April 05, 2024"},
+  {type: "Outside Mass", requestor: "John Doe", dateRequested: "April 01, 2024"},
   {type: "Wake Mass", requestor: "John Doe", dateRequested: "April 05, 2024"},
   {type: "Outside Mass", requestor: "John Doe", dateRequested: "April 01, 2024"},
 ];
@@ -53,27 +55,51 @@ const StaffDashboard = () => {
             <Paper sx={{ padding: '16px', display: 'flex', flexDirection: 'column', alignItems: 'flex-start', backgroundColor: "#D9D9D9" }} >
                 <Box sx={{ display: 'flex', alignItems: 'center' }}>
                   <FontAwesomeIcon icon={faChurch} />
-                  <Typography sx={{ marginLeft: '8px' }}>Service Requests</Typography>
+                  <Typography sx={{ marginLeft: '8px', fontWeight: 'bold' }}>Service Requests</Typography>
                 </Box>
                 <Typography sx={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}>{statistics.noServiceReq}</Typography>
             </Paper>
             <Paper sx={{ padding: '16px', display: 'flex', flexDirection: 'column', alignItems: 'flex-start', backgroundColor: "#D9D9D9" }}>
                 <Box sx={{ display: 'flex', alignItems: 'center' }}>
                   <FontAwesomeIcon icon={faStamp} />
-                  <Typography sx={{ marginLeft: '8px' }}>Certificate Requests</Typography>
+                  <Typography sx={{ marginLeft: '8px', fontWeight: 'bold' }}>Certificate Requests</Typography>
                 </Box>
                 <Typography sx={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}>{statistics.noCertReq}</Typography>
             </Paper>
             <Paper sx={{ padding: '16px', display: 'flex', flexDirection: 'column', alignItems: 'flex-start', backgroundColor: "#D9D9D9" }}>
                 <Box sx={{ display: 'flex', alignItems: 'center' }}>
                   <FontAwesomeIcon icon={faHandsPraying} />
-                  <Typography sx={{ marginLeft: '8px' }}>Mass Intentions</Typography>
+                  <Typography sx={{ marginLeft: '8px', fontWeight: 'bold' }}>Mass Intentions</Typography>
                 </Box>
                 <Typography sx={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}>{statistics.noMassIntentions}</Typography>
             </Paper>
           </Box>
+          <Box sx={{ display: 'flex', marginTop: { md: '4em', xs: '2em' } }}>
+            <h1 className='text-xl font-semibold'>Upcoming Events</h1>
+          </Box>
+          <Box className="md:mt-5 xs:mt-2">
+            <Box sx={{backgroundColor: "#355173", padding: "8px", display: "flex", alignItems: "center" }} className="gap-2">
+                <Typography sx={{ display: 'flex', justifyContent: 'flex-end', width: '100%', color: 'whitesmoke' }}>See More</Typography>
+                <FontAwesomeIcon icon={faArrowRight} className='text-white md:mr-5' />
+            </Box>
+            <Box sx={{border:'solid 1px', maxHeight: '400px', overflowY: 'auto'}}>
+              <Container maxWidth="lg">
+                {
+                  recentServices.map((service, index) => (
+                    <Paper sx={{ padding: '16px', marginBottom: '16px', backgroundColor: "#F5F5F5" }} key={index}>
+                      <Typography variant="h6">{service.type}</Typography>
+                      <Typography variant="body1">Requested by: {service.requestor}</Typography>
+                      <Typography variant="body2">Date: {service.dateRequested}</Typography>
+                    </Paper>
+                  ))
+                }
+              </Container>
+            </Box>
+          </Box>
       </Box>
+      
     </Box>
+    
   );
 }
 
