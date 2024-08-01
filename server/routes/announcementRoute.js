4d0fc7f0f3e3ac0084d880b0edab545a591acd7f
b1gmcdaddy <bobtangpuz@gmail.com>
2024-08-02 02:22:57 +0800

    added: edit function in announcement controller

--- server/routes/announcementRoute.js
+++ server/routes/announcementRoute.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const { retrieveAll, createAnnouncement } = require('../controllers/announcementController')
+const { retrieveAll, createAnnouncement, editAnnouncement } = require('../controllers/announcementController')
 
-router.get('/create', createAnnouncement)
+router.post('/create', createAnnouncement)
 router.get('/retrieve-all', retrieveAll)
+router.put('/edit/:id', editAnnouncement)
 
 module.exports = router
