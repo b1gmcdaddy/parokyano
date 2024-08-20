6673b1ec07ed290db47f70128c9737bc1f87f7c0
b1gmcdaddy <bobtangpuz@gmail.com>
2024-08-20 19:35:04 +0800

    create user controller (to implement hashing of pass)

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const db = require('./db')
 
+
 const retrieveAllUsers = (req, res) => {
     db.query('Select * FROM user', (err, result) => {
         if(err){
@@ -16,7 +17,25 @@ const retrieveAllUsers = (req, res) => {
     })
 }
 
+const dateToday = new Date().toJSON().slice(0,10)
+
+const createUser = (req, res) => {
+    const data = req.body;
+
+    db.query('INSERT INTO user (first_name, last_name, user_type, date_started, contact_no, email, username, password, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+        [data.first_name, data.last_name, data.user_type, dateToday, data.contact_no, data.email, data.username, data.password, data.status], 
+        (err, result) => {
+            if (err) {
+                console.error('error submitting to db', err)
+                return res.status(500)
+            }
+            return res.status(200).json({ message: 'User created successfully' });
+        }
+    )
+}
+
 
 module.exports = {
-    retrieveAllUsers
+    retrieveAllUsers,
+    createUser,
 }
