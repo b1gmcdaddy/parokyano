a8f508915b6c42f411632e06911d1ff3a9a3a736
CJ <noobclyde21@gmail.com>
2024-08-21 21:43:59 +0800

    table layout for staff

--- client/src/pages/staff/intentions-list/IntentionsApproved.jsx
+++ client/src/pages/staff/intentions-list/IntentionsApproved.jsx
@@ -1,4 +1,4 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Paper, Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Button } from "@mui/material";
 import React from "react";
 
 const dummyData = [
@@ -27,82 +27,117 @@ const dummyData = [
 
 const IntentionsApproved = () => {
     return(
-        <>
-        <TableContainer  sx={{ borderRadius: '16px', overflow: 'hidden' }}>
-            <Table stickyHeader aria-label="custom table">
-                <TableHead>
-                    <TableRow sx={{borderRadius: 10}}>
-                        <TableCell>type</TableCell>
-                        <TableCell>offered_by</TableCell>
-                        <TableCell>method</TableCell>
-                        <TableCell>schedule</TableCell>
-                        <TableCell>transaction_no</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {dummyData.map((data) => (
-                        <TableRow
-                            sx={{
-                                backgroundColor: '#e0e0e0',
-                                borderRadius: '8px',
-                                '& > *': {
-                                    borderBottom: 'none',
-                                },
-                            }}
-                        >
-                            <TableCell
-                                sx={{
-                                    padding: '16px',
-                                    textAlign: 'center', 
-                                    borderRadius: '12px 0 0 12px', 
-                                    backgroundColor: '#e0e0e0',
-                                }}
-                            >
-                            {data.type}
-                            </TableCell>
-                            <TableCell
-                                sx={{
-                                    padding: '16px',
-                                    textAlign: 'center', 
-                                    backgroundColor: '#e0e0e0',
-                                }}
-                            >
-                            {data.offered_by}
-                            </TableCell>
-                            <TableCell
-                                sx={{
-                                    padding: '16px',
-                                    textAlign: 'center',
-                                    backgroundColor: '#e0e0e0',
-                                }}
-                            >
-                            {data.payment_method}
-                            </TableCell>
-                            <TableCell
-                                sx={{
-                                    padding: '16px',
-                                    textAlign: 'center',
-                                    backgroundColor: '#e0e0e0',
-                                }}
-                            >
-                            {data.schedule}
-                            </TableCell>
-                            <TableCell
-                                sx={{
-                                    padding: '16px',
-                                    textAlign: 'center',
-                                    borderRadius: '0 12px 12px 0',
-                                    backgroundColor: '#e0e0e0',
-                                }}
-                            >
-                            {data.transaction_no}
-                            </TableCell>
+        <div style={{margin: '0 auto'}}>
+            <TableContainer  sx={{ display: 'flex', borderRadius: '16px', overflow: 'hidden', border: 'none', }}>
+                <Table stickyHeader aria-label="custom table" sx={{borderCollapse: 'separate', borderSpacing: 0, minWidth: 1000,}}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell sx={{textAlign: 'center'}}>type</TableCell>
+                            <TableCell sx={{textAlign: 'center'}}>offered_by</TableCell>
+                            <TableCell sx={{textAlign: 'center'}}>method</TableCell>
+                            <TableCell sx={{textAlign: 'center'}}>schedule</TableCell>
+                            <TableCell sx={{textAlign: 'center'}}>transaction_no</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-        </>
+                    </TableHead>
+                    <TableBody>
+                        {dummyData.map((data,index) => (
+                            <React.Fragment key={index}>
+                                {/* this is to add space in between rows sa table */}
+                                <TableRow>
+                                    <TableCell colSpan={5} sx={{ backgroundColor: '#ffffff', padding: 0 }}>
+                                        <Box sx={{ height: '5px', backgroundColor: 'white',}} />
+                                    </TableCell>
+                                </TableRow>
+
+                                <TableRow
+                                    sx={{
+                                        backgroundColor: '#e0e0e0',
+                                        borderRadius: '10px',
+                                        '& > *': {
+                                            borderBottom: 'none',
+                                        },
+                                    }}
+                                >
+                                    <TableCell
+                                        sx={{
+                                            border: 'none',
+                                            padding: '16px',
+                                            textAlign: 'center', 
+                                            borderRadius: '15px 0 0 15px', 
+                                            backgroundColor: '#e0e0e0',
+                                        }}
+                                    >
+                                        {data.type}
+                                    </TableCell>
+                                    <TableCell
+                                        sx={{
+                                            border: 'none',
+                                            padding: '16px',
+                                            textAlign: 'center', 
+                                            backgroundColor: '#e0e0e0',
+                                        }}
+                                    >
+                                        {data.offered_by}
+                                    </TableCell>
+                                    <TableCell
+                                        sx={{
+                                            border: 'none',
+                                            padding: '16px',
+                                            textAlign: 'center',
+                                            backgroundColor: '#e0e0e0',
+                                        }}
+                                    >
+                                        {data.payment_method}
+                                    </TableCell>
+                                    <TableCell
+                                        sx={{
+                                            border: 'none',
+                                            padding: '16px',
+                                            textAlign: 'center',
+                                            backgroundColor: '#e0e0e0',
+                                        }}
+                                    >
+                                        {data.schedule}
+                                    </TableCell>
+                                    <TableCell
+                                        sx={{
+                                            border: 'none',
+                                            padding: '16px',
+                                            textAlign: 'center',
+                                            backgroundColor: '#e0e0e0',
+                                        }}
+                                    >
+                                        {data.transaction_no}
+                                    </TableCell>
+                                    <TableCell 
+                                        sx={{
+                                            border: 'none',
+                                            padding: '16px',
+                                            textAlign: 'center',
+                                            borderRadius: '0 15px 15px 0',
+                                            backgroundColor: '#e0e0e0',
+                                        }}
+                                    >
+                                        <Button 
+                                            sx={{
+                                                backgroundColor: '#355173', 
+                                                color: 'white', 
+                                                borderRadius: '10px',
+                                                '&:hover': {
+                                                    backgroundColor: '#0036B1'
+                                                }
+                                            }}
+                                        >
+                                            INFO    
+                                        </Button>
+                                    </TableCell>
+                                </TableRow>
+                            </React.Fragment>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
     )
 }
 
