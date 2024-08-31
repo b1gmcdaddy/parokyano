3dcd0c5c64028674570154062eb2a8c391e1dd36
b1gmcdaddy <bobtangpuz@gmail.com>
2024-09-01 01:57:04 +0800

    generate reports all category

--- client/src/components/StaffReport.jsx
+++ client/src/components/StaffReport.jsx
@@ -1,55 +1,70 @@
-import { Box, Typography, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Button } from "@mui/material";
-import React, {useState} from "react";
+import { Box, Typography, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import React, { useState, useEffect } from "react";
 import logo from '../assets/logo.png';
-
-
+import axios from "axios";
+import config from "../config";
+import { ReactToPrint } from 'react-to-print';
 
 const StaffReport = () => {
 
-    const [reportInfo, setReportInfo] = useState([]);
+    const [reportInfo, setReportInfo] = useState({});
 
+    useEffect(() => {
+        const getSummaryReport = async () => {
+            try {
+                const res = await axios.get(`${config.API}/request/summary`, {
+                    params: {
+                        requestDate: '2024-07-01',  // for testing muna
+                        approveDate: '2024-07-20' 
+                    }
+                });
+                setReportInfo(res.data); 
+            } catch (err) {
+                console.error('error retrieving summary', err);
+            }
+        };
+        getSummaryReport();
+    }, []);
 
-  return (
-    <>
-        <Box sx={{ display: "flex", alignItems: 'center' }}>
-            <img src={logo} style={{ height: 'auto', width: '10%'}} />
-            <Typography>Catholic Church of Christ of the Agony <br/>Gethsemane Parish</Typography>
-        </Box>
-        <Box sx={{ textAlign: 'center', margin: 'auto' }}>
-            <Typography>Parokyano Generated Report</Typography>
-            <Typography sx={{ fontStyle: 'italic', fontSize: '14px'}}>`Date Here`</Typography>
-        </Box>
+    return (
+        <>
+            <Box sx={{ display: "flex", alignItems: 'center' }}>
+                <img src={logo} style={{ height: 'auto', width: '10%' }} alt="Logo" />
+                <Typography>Catholic Church of Christ of the Agony <br/>Gethsemane Parish</Typography>
+            </Box>
+            <Box sx={{ textAlign: 'center', margin: 'auto' }}>
+                <Typography>Parokyano Generated Report</Typography>
+                <Typography sx={{ fontStyle: 'italic', fontSize: '14px' }}>{'Date Here'}</Typography>
+            </Box>
 
-        <Box sx={{marginTop: '3em'}}>
+            <Box sx={{ marginTop: '3em' }}>
                 <TableContainer component={Paper}>
                     <Table sx={{ minWidth: 450 }} aria-label="simple table">
                         <TableHead>
-                        <TableRow sx={{backgroundColor: '#D9D9D9'}}>
-                            <TableCell sx={{fontWeight: 'bold', textTransform: 'uppercase'}}>Parish Service</TableCell>
-                            <TableCell align="right" sx={{fontWeight: 'bold'}}>Pending Requests</TableCell>
-                            <TableCell align="right" sx={{fontWeight: 'bold'}}>Cancelled Requests</TableCell>
-                            <TableCell align="right" sx={{fontWeight: 'bold'}}>Approved Requests</TableCell>
-                            <TableCell align="right" sx={{fontWeight: 'bold'}}>Fees Collected</TableCell>
-                        </TableRow>
+                            <TableRow sx={{backgroundColor: '#D9D9D9'}}>
+                                <TableCell sx={{fontWeight: 'bold', textTransform: 'uppercase'}}>Parish Service</TableCell>
+                                <TableCell align="right" sx={{fontWeight: 'bold'}}>Pending Requests</TableCell>
+                                <TableCell align="right" sx={{fontWeight: 'bold'}}>Cancelled Requests</TableCell>
+                                <TableCell align="right" sx={{fontWeight: 'bold'}}>Approved Requests</TableCell>
+                                <TableCell align="right" sx={{fontWeight: 'bold'}}>Fees Collected</TableCell>
+                            </TableRow>
                         </TableHead>
                         <TableBody>
-
-                            <TableRow>
-                            <TableCell component="th" scope="row">
-      
-                            </TableCell>
-                            <TableCell align="right"></TableCell>
-                            <TableCell align="right"></TableCell>
-                            <TableCell align="right"></TableCell>
-                            <TableCell align="right"></TableCell>
-                            </TableRow>
-              
+                            {Object.entries(reportInfo).map(([type, info]) => (
+                                <TableRow key={type} sx={{ '&:last-child td, &:last-child th': { border: 0 } }} >
+                                    <TableCell component="th" scope="row">{type}</TableCell>
+                                    <TableCell align="right">{info.pending}</TableCell>
+                                    <TableCell align="right">{info.cancelled}</TableCell>
+                                    <TableCell align="right">{info.approved}</TableCell>
+                                    <TableCell align="right"> {/* to implement */} </TableCell>
+                                </TableRow>
+                            ))}
                         </TableBody>
                     </Table>
                 </TableContainer>
-        </Box>
-    </>
-  )
+            </Box>
+        </>
+    );
 };
 
 export default StaffReport;
--- client/src/pages/staff/GenerateReports.jsx
+++ client/src/pages/staff/GenerateReports.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import NavStaff from '../../components/NavStaff';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -6,8 +6,7 @@ import { Grid, Typography, TextField, Button, MenuItem, Container } from '@mui/m
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFileExport, faPrint } from '@fortawesome/free-solid-svg-icons';
 import StaffReport from '../../components/StaffReport';
-
-
+import ReactToPrint from 'react-to-print';
 
 const GenerateReports = () => {
 
@@ -21,62 +20,66 @@ const GenerateReports = () => {
         setReportDetails({ ...reportDetails, [e.target.name]: e.target.value});
     };
 
-  return (
-    <Box sx={{ display: 'flex', mx: { md: '30px' } }}>
-        <NavStaff />
-        <Box component="main" sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${240}px)` } }} >
-            <Toolbar />
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', 
-                marginTop: '8px', alignItems: 'center', marginBottom: '1.5em' }}>
-                <Typography sx={{fontSize: "1.25rem", lineHeight: "1.75rem", fontWeight: 600}}>Generate Report</Typography> 
-            </Box>
-            <Grid container spacing={2}>
-                <Grid item xs={12} sm={3}>
-                    <label>Start Date:</label>
-                    <TextField type="date" size="small" name="startDate" variant="outlined" value={reportDetails.startDate} onChange={handleChange} InputLabelProps={{ shrink: true }} fullWidth required />
-                </Grid>
-                <Grid item xs={12} sm={3}>
-                    <label>End Date:</label>
-                    <TextField type="date" size="small" name="endDate" variant="outlined" value={reportDetails.endDate} onChange={handleChange} InputLabelProps={{ shrink: true }} fullWidth required />
-                </Grid>
-                <Grid item xs={12} sm={3}>
-                    <label>Category: </label>
-                    <TextField select variant="outlined" name="category" onChange={handleChange} value={reportDetails.category} size="small" fullWidth required>
-                      <MenuItem value="All">All</MenuItem>
-                      <MenuItem value="Baptism">Baptism</MenuItem>
-                      <MenuItem value="Wedding">Wedding</MenuItem>
-                      <MenuItem value="Anointing">Anointing of the Sick</MenuItem>
-                      <MenuItem value="Funeral-Mass">Funeral Mass</MenuItem>
-                      <MenuItem value="Outside-Mass">Outside Mass</MenuItem>
-                      <MenuItem value="Wake-Mass">Wake Mass</MenuItem>
-                      <MenuItem value="Blessing">Blessing</MenuItem>
-                      <MenuItem value="Baptismal-Certificate">Baptismal Certificate</MenuItem>
-                      <MenuItem value="Wedding-Certificate">Wedding Certificate</MenuItem>
-                      </TextField>
-                </Grid>
-                <Grid item xs={12} sm={3}>
-                    <Button sx={{backgroundColor: '#F5F5F5', color: '#355173', border: '.1px solid', marginTop: { sm: '1.6em' }}}>GENERATE</Button>
-                </Grid>
-            </Grid>
+    const componentRef = useRef();
 
-            <Box className="md:mt-14 xs:mt-14">
-                <Box sx={{backgroundColor: "#355173", padding: "12px", display: "flex", alignItems: "center", color: 'white', justifyContent: 'space-between' }} className="gap-2">
-                    <Typography sx={{ width: '100%', color: 'whitesmoke' }}>{reportDetails.startDate} &nbsp;-&nbsp; {reportDetails.endDate}</Typography>
-                    <Typography>Export</Typography>
-                    <FontAwesomeIcon icon={faFileExport} className='text-white md:mr-5' />
-                    <Typography>Print</Typography>
-                    <FontAwesomeIcon icon={faPrint} className='text-white md:mr-5' />
+    return (
+        <Box sx={{ display: 'flex', mx: { md: '30px' } }}>
+            <NavStaff />
+            <Box component="main" sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${240}px)` } }} >
+                <Toolbar />
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', 
+                    marginTop: '8px', alignItems: 'center', marginBottom: '1.5em' }}>
+                    <Typography sx={{fontSize: "1.25rem", lineHeight: "1.75rem", fontWeight: 600}}>Generate Report</Typography> 
                 </Box>
-            <Box sx={{border:'solid 1px', maxHeight: '700px', overflowY: 'auto', backgroundColor: '#F5F5F5'}}>
-                <Container maxWidth="lg" sx={{backgroundColor: 'white',}}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={3}>
+                        <label>Start Date:</label>
+                        <TextField type="date" size="small" name="startDate" variant="outlined" value={reportDetails.startDate} onChange={handleChange} InputLabelProps={{ shrink: true }} fullWidth required />
+                    </Grid>
+                    <Grid item xs={12} sm={3}>
+                        <label>End Date:</label>
+                        <TextField type="date" size="small" name="endDate" variant="outlined" value={reportDetails.endDate} onChange={handleChange} InputLabelProps={{ shrink: true }} fullWidth required />
+                    </Grid>
+                    <Grid item xs={12} sm={3}>
+                        <label>Category: </label>
+                        <TextField select variant="outlined" name="category" onChange={handleChange} value={reportDetails.category} size="small" fullWidth required>
+                          <MenuItem value="All">All</MenuItem>
+                          <MenuItem value="Baptism">Baptism</MenuItem>
+                          <MenuItem value="Wedding">Wedding</MenuItem>
+                          <MenuItem value="Anointing">Anointing of the Sick</MenuItem>
+                          <MenuItem value="Funeral-Mass">Funeral Mass</MenuItem>
+                          <MenuItem value="Outside-Mass">Outside Mass</MenuItem>
+                          <MenuItem value="Wake-Mass">Wake Mass</MenuItem>
+                          <MenuItem value="Blessing">Blessing</MenuItem>
+                          <MenuItem value="Baptismal-Certificate">Baptismal Certificate</MenuItem>
+                          <MenuItem value="Wedding-Certificate">Wedding Certificate</MenuItem>
+                          </TextField>
+                    </Grid>
+                    <Grid item xs={12} sm={3}>
+                        <Button sx={{backgroundColor: '#F5F5F5', color: '#355173', border: '.1px solid', marginTop: { sm: '1.6em' }}}>GENERATE</Button>
+                    </Grid>
+                </Grid>
+
+                <Box className="md:mt-14 xs:mt-14">
+                    <Box sx={{backgroundColor: "#355173", padding: "12px", display: "flex", alignItems: "center", color: 'white', justifyContent: 'space-between' }} className="gap-2">
+                        <Typography sx={{ width: '100%', color: 'whitesmoke' }}>{reportDetails.startDate} &nbsp;-&nbsp; {reportDetails.endDate}</Typography>
+                        <Typography>Export</Typography>
+                        <FontAwesomeIcon icon={faFileExport} className='text-white md:mr-5' />
+                        <ReactToPrint trigger={() => (
+                                <Button sx={{color: 'white'}}>Print<FontAwesomeIcon icon={faPrint} className='text-white md:mr-5 md:ml-2' /></Button>   
+                            )} content={() => componentRef.current}
+                        />
+                    </Box>
+                <Box sx={{border:'solid 1px', maxHeight: '700px', overflowY: 'auto', backgroundColor: '#F5F5F5'}} ref={componentRef}>
+                    <Container maxWidth="lg" sx={{backgroundColor: 'white',}}>
                         <StaffReport />
-                </Container>
+                    </Container>
+                </Box>
+              </Box>
             </Box>
-          </Box>
         </Box>
-    </Box>
-    
-  );
+        
+    );
 };
 
 export default GenerateReports;
