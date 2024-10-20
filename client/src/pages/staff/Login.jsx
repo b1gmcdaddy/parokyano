bff16910d396765214f7eb69f66f4198fab8d6b3
b1gmcdaddy <bobtangpuz@gmail.com>
2024-10-21 00:27:45 +0800

    fixed login not navigating to dashboard bug

--- client/src/pages/staff/Login.jsx
+++ client/src/pages/staff/Login.jsx
@@ -1,16 +1,9 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import StaffAppBar from "../../components/StaffAppBar";
-import {
-  Box,
-  Paper,
-  TextField,
-  Button,
-  Typography,
-  Alert,
-} from "@mui/material";
+import {Box, Paper, TextField, Button, Typography, Alert} from "@mui/material";
 import logo from "../../assets/logo.png";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import config from "../../config";
 
 const Login = () => {
@@ -22,7 +15,7 @@ const Login = () => {
   });
 
   const handleChange = (e) => {
-    setLoginData({ ...loginData, [e.target.name]: e.target.value });
+    setLoginData({...loginData, [e.target.name]: e.target.value});
   };
 
   const handleSubmit = async (e) => {
@@ -37,7 +30,7 @@ const Login = () => {
         console.log(response.data);
 
         if (response.data.role === "staff" || response.data.role === "admin") {
-          navigate("/dashboard", { replace: true });
+          navigate("/dashboard", {replace: true});
         }
       }
     } catch (err) {
@@ -58,50 +51,45 @@ const Login = () => {
           justifyContent: "center",
           alignItems: "center",
           position: "relative",
-        }}
-      >
+        }}>
         <Box
           sx={{
             position: "absolute",
             top: "8%",
             display: "flex",
             justifyContent: "center",
-          }}
-        >
+          }}>
           <img
             src={logo}
             alt="Logo"
-            style={{ width: "220px", height: "200px" }}
+            style={{width: "220px", height: "200px"}}
           />
         </Box>
         <Paper
           elevation={2}
           sx={{
-            padding: { md: 6, xs: 6 },
+            padding: {md: 6, xs: 6},
             display: "flex",
             flexDirection: "column",
             gap: 2,
             width: "100%",
             maxWidth: "600px",
             marginTop: "50px",
-          }}
-        >
+          }}>
           <Typography
             sx={{
               fontWeight: "bold",
-              fontSize: { md: "1.55em", xs: "1.25em" },
+              fontSize: {md: "1.55em", xs: "1.25em"},
               textAlign: "center",
-            }}
-          >
+            }}>
             Gethsemane Parish Church
           </Typography>
           <Typography
             sx={{
               textAlign: "center",
-              marginBottom: { md: "1.55em" },
-              fontSize: { md: "14px", xs: "10px" },
-            }}
-          >
+              marginBottom: {md: "1.55em"},
+              fontSize: {md: "14px", xs: "10px"},
+            }}>
             Please login to your account
           </Typography>
           {error && <Alert severity="error">{error}</Alert>}{" "}
@@ -113,7 +101,7 @@ const Login = () => {
               variant="outlined"
               fullWidth
               onChange={handleChange}
-              sx={{ marginBottom: "12px " }}
+              sx={{marginBottom: "12px "}}
             />
             <TextField
               label="Password"
@@ -126,13 +114,12 @@ const Login = () => {
             <Button
               variant="contained"
               fullWidth
-              sx={{ backgroundColor: "#355173", marginTop: "12px" }}
-              type="submit"
-            >
+              sx={{backgroundColor: "#355173", marginTop: "12px"}}
+              type="submit">
               Login
             </Button>
           </form>
-          <Typography sx={{ textAlign: "center", marginTop: "16px" }}>
+          <Typography sx={{textAlign: "center", marginTop: "16px"}}>
             Forgot Password?
           </Typography>
         </Paper>
