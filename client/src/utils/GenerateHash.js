4d656e617a685e1d97c8a70c122c4af8b910c9a1
CJ <noobclyde21@gmail.com>
2024-11-25 08:26:29 +0800

    test

--- client/src/utils/GenerateHash.js
+++ client/src/utils/GenerateHash.js
@@ -1,21 +1,25 @@
 import axios from "axios";
 import config from "../config"; // Assuming you have a config file for API URLs
 
-export default async function generateHash() {
-  try {
+export default function generateHash() {
+  const fetchIntentionCount = async () => {
     const response = await axios.get(
       `${config.API}/configuration/intentionCount`
     );
+    console.log(response.data);
 
     if (response.status === 200) {
-      const intentionCount = response.data.intentionCount;
+      const intentionCount = response.data;
+      console.log(response.data);
       return intentionCount;
     } else {
       console.error("Failed to fetch intention count");
       return null;
     }
-  } catch (error) {
-    console.error("Error fetching intention count:", error);
-    return null;
-  }
+    // } catch (error) {
+    //   console.error("Error fetching intention count:", error);
+    //   return null;
+    // }
+  };
+  fetchIntentionCount();
 }
